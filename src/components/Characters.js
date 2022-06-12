bae16d7888b30de6e526f3969cd68b33f7bbbb8f
Nerio Loroña Mayta <neriolorona@MacBook-Pro-de-Nerio.local>
2022-06-12 19:25:59 -0400

    controlling view for loading characters

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -7,18 +7,43 @@ const URL_API = 'https://rickandmortyapi.com/api'
 
 const Characters = () => {
   const {darkMode} = useContext(ThemeContext)
+  const [isLoading, setIsLoading] = useState(false)
   const [characters, setCharacters] = useState([])
 
-  useEffect(() => {
+  const getCharacters = () => {
+    setIsLoading(true)
     fetch(`${URL_API}/character`)
       .then(response => response.json() )
-      .then(data => setCharacters(data.results))
+      .then(data => {
+        setCharacters(data.results)
+        setIsLoading(false)
+      })
+      .catch(error => {
+        console.log('Error getting characters', error)
+        setIsLoading(false)
+      })
+  }
+
+  useEffect(() => {
+    getCharacters()
   }, [])
 
+  const renderCharactes = () => {
+    if(isLoading) {
+      return <p>Cargando...</p>
+    }
+
+    return (
+      <>
+        {characters.map(character => <Character key={character.id} {...character} />)}
+      </>
+    )
+  }
+
   return (
-    <div className={clsx('mx-auto mt-4 grid justify-items-center items-center gap-6 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6', {'bg-black': darkMode, 'bg-white': !darkMode})} >
-      {characters.map(character => <Character {...character} />)}
-    </div>
+    <section className={clsx('p-4 grid justify-items-center items-center gap-6 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6', {'bg-black': darkMode, 'bg-white': !darkMode})} >
+      {renderCharactes()}
+    </section>
   )
 }
 
