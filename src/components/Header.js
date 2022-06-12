f1840ad27d4cc42e1c373166536c4b51ac2b25eb
Nerio Loroña Mayta <neriolorona@MacBook-Pro-de-Nerio.local>
2022-06-12 18:51:25 -0400

    responsive header

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,9 +6,12 @@ function Header() {
   const {darkMode, handleDarkMode} = useContext(ThemeContext)
 
   return (
-    <div className={clsx('flex justify-between items-center mx-auto h-24 px-24', {'bg-black' : darkMode, 'bg-white': !darkMode})} >
-      <h3 className={clsx('font-bold', {'text-white': darkMode, 'text-black': !darkMode})} >Reak Hooks</h3>
-      <button className="border-2 border-solid py-2 px-6 rounded-md hover:bg-violet-600 hover:text-white" onClick={handleDarkMode} >{darkMode ? 'Light' : 'Dark'}</button>
+    <div className={clsx('py-2 border-b-2 text-center md:flex md:justify-between md:items-center md:px-12', {'bg-black' : darkMode, 'bg-white': !darkMode})} >
+      <div>
+        <p className={clsx('font-bold text-xs', {'text-white': darkMode, 'text-black': !darkMode})} >Reak Hooks</p>
+        <h3 className={clsx('font-bold', {'text-white': darkMode, 'text-black': !darkMode})} >Rick and Morty</h3>
+      </div>
+      <button className={clsx('border-2 border-solid py-2 px-8 mt-2 rounded-md hover:bg-violet-600 hover:text-white', {'text-white': darkMode, 'text-black': !darkMode})} onClick={handleDarkMode} >{darkMode ? 'Light' : 'Dark'}</button>
     </div>
   )
 }
